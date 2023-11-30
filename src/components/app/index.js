7d328224def5e4f2b1a156d1ac780fc824298bd0
Breno de Freitas Mendes <brenofreitasmendes@gmail.com>
2023-11-30 00:30:28 -0300

    feat: ListContacts Header

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,15 +1,23 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/deafault';
-import { Container } from './styles';
+
 import Header from '../header';
+import ConctactsList from '../contactsList';
+
+import { Container } from './styles'
 
 function App() {
   return (
-    <>
-      <GlobalStyles theme={defaultTheme}/>
-      <Header/>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+
+      <Container>
+        <Header />
+        <ConctactsList />
+      </Container>
+    </ThemeProvider>
   );
 }
 
