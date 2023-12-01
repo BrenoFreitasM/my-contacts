8f18e54d34ae13c456d11b82402d29789fd5d75b
BrenoFreitasM <brenofreitasmendes@gmail.com>
2023-11-30 22:31:25 -0300

    feat: finished contacts part

--- src/components/contactsList/index.js
+++ src/components/contactsList/index.js
@@ -1,5 +1,7 @@
 import { Container, Header, ListContainer, Card } from "./styles";
 import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
 
 export default function ConctactsList() {
   return (
@@ -17,7 +19,61 @@ export default function ConctactsList() {
         </header>
 
         <Card>
-          ...
+          <div className="info">
+            <div className="contact-name">
+              <strong>Breno Freitas</strong>
+              <small>Instagram</small>
+            </div>
+            <span>brenofreitasmendes@gmail.com</span>
+            <span>(35) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <a href="/">
+                <img src={edit} alt="edit"/>
+              </a>
+              <button type="button">
+                <img src={trash} alt="trash"/>
+              </button>
+            </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Breno Freitas</strong>
+              <small>Instagram</small>
+            </div>
+            <span>brenofreitasmendes@gmail.com</span>
+            <span>(35) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <a href="/">
+                <img src={edit} alt="edit"/>
+              </a>
+              <button type="button">
+                <img src={trash} alt="trash"/>
+              </button>
+            </div>
+        </Card>
+         <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Breno Freitas</strong>
+              <small>Instagram</small>
+            </div>
+            <span>brenofreitasmendes@gmail.com</span>
+            <span>(35) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <a href="/">
+                <img src={edit} alt="edit"/>
+              </a>
+              <button type="button">
+                <img src={trash} alt="trash"/>
+              </button>
+            </div>
         </Card>
       </ListContainer>
     </Container>
